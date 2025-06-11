c2415e4edc8b2c5e659af0b2ac2605ffe7c200dd
DYSTOpyy <bcd.saltel@gmail.com>
2025-06-12 00:33:52 +0200

    chore: update dependencies in package.json
    - Added new dependencies:
      - @llm-ui/code@^0.13.3
      - @llm-ui/markdown@^0.13.3
      - @llm-ui/react@^0.13.3
      - html-react-parser@^5.2.5
      - react-markdown@^10.1.0
      - remark-gfm@^4.0.1
      - shiki@^3.6.0

--- front-build/components/ui/custom/MessageIA.tsx
+++ front-build/components/ui/custom/MessageIA.tsx
@@ -1,4 +1,13 @@
 import React from "react";
+import {
+  codeBlockLookBack,
+  findCompleteCodeBlock,
+  findPartialCodeBlock,
+} from "@llm-ui/code";
+import { markdownLookBack } from "@llm-ui/markdown";
+import { throttleBasic, useLLMOutput, useStreamExample } from "@llm-ui/react";
+import { MarkdownComponent } from "@/components/ui/custom/MarkdownComponent";
+import { CodeComponent } from "@/components/ui/custom/CodeComponent";
 
 interface MessageIAProps extends React.PropsWithChildren {
   message: string;
@@ -9,11 +18,25 @@ const MessageIA: React.FC<MessageIAProps> = ({ message }) => {
     navigator.clipboard.writeText(message);
   };
 
+  const { isStreamFinished, output } = useStreamExample(message);
+
+  const { blockMatches } = useLLMOutput({
+    llmOutput: message,
+    fallbackBlock: {
+      component: MarkdownComponent, // from Step 1
+      lookBack: markdownLookBack(),
+    },
+    isStreamFinished,
+  });
+
   return (
     <div className="flex flex-row gap-5 justify-start my-3 text-lg w-[90%]">
       <img src="/images/logo-couleur.png" alt="" className="size-10" />
       <div className="flex flex-col gap-3">
-        <p>{message}</p>
+        {blockMatches.map((blockMatch, index) => {
+          const Component = blockMatch.block.component;
+          return <Component key={index} blockMatch={blockMatch} />;
+        })}
         <img
           src="/images/copy.png"
           alt=""
