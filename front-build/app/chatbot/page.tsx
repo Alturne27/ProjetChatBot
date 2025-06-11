262486dd644f4d815e7ed919be5ae44ff181c04a
DYSTOpyy <bcd.saltel@gmail.com>
2025-06-11 23:30:26 +0200

    feat: integrate loading component and enhance session loading experience in Sidebar and Home

--- front-build/app/chatbot/page.tsx
+++ front-build/app/chatbot/page.tsx
@@ -9,12 +9,13 @@ import MessageUser from "@/components/ui/custom/MessageUser";
 import MessageIA from "@/components/ui/custom/MessageIA";
 import ChatHistory from "@/components/ui/custom/ChatHistory";
 import SignIn from "@/components/auth/sign-in";
+import Loading from "@/components/ui/custom/Loading";
 
 export default function Home() {
   const [isPanelVisible, setPanelVisible] = useState(true);
   const sideBarMobileRef = useRef<HTMLDivElement>(null);
   const mainPageRef = useRef<HTMLDivElement>(null);
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
   const [isResponseLoading, setIsResponseLoading] = useState(false);
 
   const [newChatSession, setNewChatSession] = useState<ChatSession | null>(
@@ -40,7 +41,15 @@ export default function Home() {
     }
   }, [isPanelVisible]);
 
-  if (!session) {
+  if (status === "loading") {
+    return (
+      <div className="flex flex-col items-center justify-center h-screen w-screen">
+        <Loading />
+      </div>
+    );
+  }
+
+  if (status === "unauthenticated") {
     return (
       <div className="flex flex-col items-center justify-center h-screen">
         <p className="text-2xl text-white font-title">
@@ -51,64 +60,65 @@ export default function Home() {
         </SessionProvider>
       </div>
     );
-  } else {
-    return (
-      <div className="flex">
-        <div className="absolute z-50 " ref={sideBarMobileRef}>
-          <Sidebar
-            isPanelVisible={isPanelVisible}
-            setPanelVisible={setPanelVisible}
-            setNewChatSession={setNewChatSession}
-            newChatSession={newChatSession}
-            setChatSessions={setChatSessions}
-            chatSessions={chatSessions}
+  }
+  return (
+    <div className="flex">
+      <div className="absolute z-50 " ref={sideBarMobileRef}>
+        <Sidebar
+          isPanelVisible={isPanelVisible}
+          setPanelVisible={setPanelVisible}
+          setNewChatSession={setNewChatSession}
+          newChatSession={newChatSession}
+          setChatSessions={setChatSessions}
+          chatSessions={chatSessions}
+        />
+      </div>
+
+      <main
+        ref={mainPageRef}
+        className="main-page flex-1 bg-white h-screen justify-center flex flex-col"
+      >
+        <div
+          className="absolute top-0 left-0 justify-between items-center w-full p-5 transition-all duration-200 flex-shrink-0 bg-transparent"
+          style={{ opacity: isPanelVisible ? 0 : 1 }}
+        >
+          <img
+            src="images/menu_ouvrir.png"
+            alt=""
+            className="w-8 h-8 cursor-pointer"
+            onClick={() => {
+              setPanelVisible(!isPanelVisible);
+            }}
           />
         </div>
-
-        <main
-          ref={mainPageRef}
-          className="main-page flex-1 bg-white h-screen justify-center flex flex-col"
-        >
-          <div
-            className="absolute top-0 left-0 justify-between items-center w-full p-5 transition-all duration-200 flex-shrink-0 bg-transparent"
-            style={{ opacity: isPanelVisible ? 0 : 1 }}
-          >
-            <img
-              src="images/menu_ouvrir.png"
-              alt=""
-              className="w-8 h-8 cursor-pointer"
-              onClick={() => {
-                setPanelVisible(!isPanelVisible);
-              }}
-            />
+        {newChatSession === null ? (
+          <div className="flex items-center justify-center h-20">
+            <p className="text-white text-xl font-title">
+              Bienvenue dans votre nouveau chat !
+            </p>
           </div>
-          {newChatSession === null ? (
-            <div className="flex items-center justify-center h-20">
-              <p className="text-white text-xl font-title">Bienvenue dans votre nouveau chat !</p>
-            </div>
-          ) : (
-            <div className="flex-1 overflow-y-scroll px-4">
-              <div className="max-w-4xl mx-auto space-y-4 py-4">
-                <ChatHistory
-                  session={newChatSession}
-                  isResponseLoading={isResponseLoading}
-                />
-              </div>
-            </div>
-          )}
-
-          <div className="flex-shrink-0 max-w-4xl mx-auto w-full px-4 pb-4">
-            <SessionProvider>
-              <Askbar
-                newChatSession={newChatSession}
-                setNewChatSession={setNewChatSession}
-                setChatSessions={setChatSessions}
-                setIsResponseLoading={setIsResponseLoading}
+        ) : (
+          <div className="flex-1 overflow-y-scroll px-4">
+            <div className="max-w-4xl mx-auto space-y-4 py-4">
+              <ChatHistory
+                session={newChatSession}
+                isResponseLoading={isResponseLoading}
               />
-            </SessionProvider>
+            </div>
           </div>
-        </main>
-      </div>
-    );
-  }
+        )}
+
+        <div className="flex-shrink-0 max-w-4xl mx-auto w-full px-4 pb-4">
+          <SessionProvider>
+            <Askbar
+              newChatSession={newChatSession}
+              setNewChatSession={setNewChatSession}
+              setChatSessions={setChatSessions}
+              setIsResponseLoading={setIsResponseLoading}
+            />
+          </SessionProvider>
+        </div>
+      </main>
+    </div>
+  );
 }
